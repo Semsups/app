d0ff1310b30de2d8459bc0dccf17e33e6f84303f
Semsups <semsups@mail.ru>
2022-08-31 17:14:45 +0300

    09js

---
+++ 09.js
@@ -0,0 +1,5 @@
+function countSheeps(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;
+    }
+
+    /** a function for checking logical values in an array and outputting their number */
