73f765535475f242ed9d2f111d6b73e92ea95728
Semsups <semsups@mail.ru>
2022-08-31 17:13:45 +0300

    08js

---
+++ 08.js
@@ -0,0 +1,8 @@
+function areYouPlayingBanjo(name) {
+    if (name[0] == 'R' || name[0] == 'r')
+    return name + " plays banjo";
+    else
+    return name + " does not play banjo";
+    }
+
+    /** The function checks the first letter of the name and, depending on the name, returns a string */
