c6c7874dd1c273cc1fd2fc9973f66d4ad744a984
Semsups <semsups@mail.ru>
2022-09-01 14:59:21 +0300

    acces

---
+++ accessing.js
@@ -0,0 +1,18 @@
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  
+  
+  let gloveBoxContents = myStorage.car.inside["glove box"];
+
+  /** Доступ к Вложенным Объектам
+Доступ к вложенным свойствам объектов можно получить, связав воедино обозначения с помощью точек или скобок. */
