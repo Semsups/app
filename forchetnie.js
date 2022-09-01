beb37a3f55a1131074b20ac9ce8eefbc6c3c0685
Semsups <semsups@mail.ru>
2022-09-01 22:09:40 +0300

    for2,3

---
+++ forchetnie.js
@@ -0,0 +1,22 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+for (let i = 1; i < 10; i+= 2) {
+  myArray.push(i);
+}
+
+/** это цикл для вызова не четных целых чисел от 1 до 9 */
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+for (let i = 0; i < 10; i+= 2) {
+  myArray.push(i);
+}
+
+
+/** это цикл для вызова четных от 0 до 8 */
