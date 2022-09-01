c4887f864162420d00be1aed01611abd342e9396
Semsups <semsups@mail.ru>
2022-09-01 16:42:51 +0300

    while

---
+++ while.js
@@ -0,0 +1,15 @@
+// Setup
+const myArray = [];
+let i = 5;
+
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+// Only change code below this line
+
+/**
+ * Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+ * Добавьте числа от 5 до 0 (включительно) в порядке убывания в myArray, используя цикл while.
+ */
+ 
