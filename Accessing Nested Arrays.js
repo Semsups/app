5374d9f4ecdb9de96ed810fadbbdba8c8838cbf1
Semsups <semsups@mail.ru>
2022-09-01 15:09:19 +0300

    tres

---
+++ Accessing Nested Arrays.js
@@ -0,0 +1,22 @@
+const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  const secondTree = myPlants[1].list[1];
+
+  /** Используя обозначение точками и скобками, установите переменную second Tree на второй элемент в деревьях, потерянных из объекта myPlants. */
